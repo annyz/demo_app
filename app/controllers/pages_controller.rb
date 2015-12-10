0dbf0aab47997c40c89bc39672c308cc892a1c4d
saasbook <saasbook@saasbook.(none)>
2015-12-10 01:26:44 -0400

    Remove default rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   def home
     @title ="Home"
   end
-
+def help
+    @title ="Help"
+  end
   def contact
  @title ="Contact"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 DemoApp::Application.routes.draw do
   resources :user1s
-
-
-  get "pages/home"
-
-  get "pages/contact"
-  get "pages/about"
-
   resources :users
-
-
+  #get "pages/contact"
+  #get "pages/about"
+  #get "pages/home"
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  #match '/', :to => 'pages#home'
+  match '/signup', :to => 'user1s#new'
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
